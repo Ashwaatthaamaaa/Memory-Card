3fa21078057f922402b28249ddbdc7f86a9b1cb2
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-01-16 14:54:00 +0530

    add Header, Scoreboard, and Content components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import Header from './components/Header/Header';
+import Scoreboard from './components/Header/Scoreboard';
+import Content from './components/Content/Content';
+import { useState} from 'react';
 
 function App() {
+  const [currScore,setCurrScore] = useState(0);
+  const [bestScore,setBestScore] = useState(0);
+
+
+
 
   return (
     <>
+
+    <Header/>
+
+    <Scoreboard currScore={currScore} bestScore={bestScore}/>
+
+    <Content currScore={currScore} setCurrScore={setCurrScore} bestScore={bestScore} setBestScore={setBestScore}/>
     </>
-  )
+
+  );
 }
 
-export default App
+export default App;
