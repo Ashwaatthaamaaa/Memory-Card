13c4bf871d13a5b16b5ab06a2e1e468a4752e61f
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-01-16 14:54:18 +0530

    add Header component with game title and instructions

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import '../../styles/header.css'
+export default function Header(){
+    return(
+        <>
+            <div className="header">
+                Anime Memory Game
+            </div>
+            <p className="rule">Test your memory! Click on each zodiac sign only once.</p>
+    
+        </>
+    )
+}
